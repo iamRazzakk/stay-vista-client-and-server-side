772697e2204d50208d2aa2350fa9c34c923c7bb3
Md Abdur Razzak ~ <rakibt23p@gmail.com>
2024-01-20 13:05:03 +0600

    Create room card and implemnet data and create categories.js file for category under navbar

---
+++ client/src/components/Rooms/Categories/Categories.jsx
@@ -0,0 +1,11 @@
+import { categories } from "./CategoriesData";
+
+const Categories = () => {
+    return (
+        <div>
+            {categories.length}
+        </div>
+    );
+};
+
+export default Categories;
