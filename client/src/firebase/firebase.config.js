c53a8f914dd91384168b486e0bd6eded040e1a56
Md Abdur Razzak ~ <rakibt23p@gmail.com>
2024-01-20 11:27:45 +0600

    install firebase and npm

--- client/src/firebase/firebase.config.js
+++ client/src/firebase/firebase.config.js
@@ -1,12 +1,29 @@
-import { initializeApp } from 'firebase/app'
+// import { initializeApp } from 'firebase/app'
 
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_apiKey,
+//   authDomain: import.meta.env.VITE_authDomain,
+//   projectId: import.meta.env.VITE_projectId,
+//   storageBucket: import.meta.env.VITE_storageBucket,
+//   messagingSenderId: import.meta.env.VITE_messagingSenderId,
+//   appId: import.meta.env.VITE_appId,
+// }
+
+// export const app = initializeApp(firebaseConfig)
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_apiKey,
-  authDomain: import.meta.env.VITE_authDomain,
-  projectId: import.meta.env.VITE_projectId,
-  storageBucket: import.meta.env.VITE_storageBucket,
-  messagingSenderId: import.meta.env.VITE_messagingSenderId,
-  appId: import.meta.env.VITE_appId,
-}
+  apiKey: "AIzaSyCakjTy3nG9MSR7XYI31k436-rtsP-9NJY",
+  authDomain: "stayvista-e4f2f.firebaseapp.com",
+  projectId: "stayvista-e4f2f",
+  storageBucket: "stayvista-e4f2f.appspot.com",
+  messagingSenderId: "259038702953",
+  appId: "1:259038702953:web:3d817d0497601d634fe1f8"
+};
 
-export const app = initializeApp(firebaseConfig)
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
--- client/src/providers/AuthProvider.jsx
+++ client/src/providers/AuthProvider.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 import { createContext, useEffect, useState } from 'react'
 import {
+
   GoogleAuthProvider,
   createUserWithEmailAndPassword,
   getAuth,
@@ -11,8 +12,7 @@ import {
   signOut,
   updateProfile,
 } from 'firebase/auth'
-import { app } from '../firebase/firebase.config'
-
+import { app } from './../firebase/firebase.config';
 export const AuthContext = createContext(null)
 const auth = getAuth(app)
 const googleProvider = new GoogleAuthProvider()
